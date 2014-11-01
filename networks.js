2e6ab6e5f524ef9de691403e2c3c0181bc97d082
FlappycoinDEV <isntritchie+flappyDEV@gmail.com>
2014-11-01 23:07:38 +0000

    Update networks.js

--- networks.js
+++ networks.js
@@ -6,29 +6,25 @@ var hex = function(hex) {
 
 exports.livenet = {
   name: 'livenet',
-  magic: hex('f9beb4d9'),
-  addressVersion: 0x00,
+  magic: hex('c1c1c1c1'),
+  addressVersion: 35,
   privKeyVersion: 128,
   P2SHVersion: 5,
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+    hash: hex('64B0E6B2A85682D82499EFB52038EA053CDD97B8890E472A3E33AB8CC4228801'),
+    merkle_root: hex('318868CF8A975F003AB0C2E89F21C3D39F004F74685A729A0B03CD71ACB000D6'),
     height: 0,
-    nonce: 2083236893,
+    nonce: 265310,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1231006505,
+    timestamp: 1392281929,
     bits: 486604799,
   },
   dnsSeeds: [
-    'seed.bitcoin.sipa.be',
-    'dnsseed.bluematt.me',
-    'dnsseed.bitcoin.dashjr.org',
-    'seed.bitcoinstats.com',
-    'seed.bitnodes.io',
-    'bitseed.xf2.org'
+    'dnsseed.flappycoin.biz',
+    'dnsseed.flap.so'
   ],
   defaultClientPort: 8333
 };
@@ -47,15 +43,15 @@ exports.testnet = {
     hash: hex('43497FD7F826957108F4A30FD9CEC3AEBA79972084E90EAD01EA330900000000'),
     merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
     height: 0,
-    nonce: 414098458,
+    nonce: 265310,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1296688602,
+    timestamp: 1392281929,
     bits: 486604799,
   },
   dnsSeeds: [
-    'testnet-seed.bitcoin.petertodd.org',
-    'testnet-seed.bluematt.me'
+    'dnsseed.flappycoin.biz',
+    'dnsseed.flap.so'
   ],
   defaultClientPort: 18333
 };
